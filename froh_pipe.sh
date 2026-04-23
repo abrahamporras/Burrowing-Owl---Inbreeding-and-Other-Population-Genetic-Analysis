ffd244bef8010ee6caaf69e2573dd953160dd72c
abrahamporras <155199451+abrahamporras@users.noreply.github.com>
2026-04-22 21:47:07 -0700

    Add script to compute and summarize ROH statistics
    This script computes runs of homozygosity (ROH) and their summary statistics for populations, merging with population data and flagging small groups.

---
+++ froh_pipe.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -euo pipefail
+
+### RUN ROH
+bcftools roh --AF-tag AF --rec-rate 1e-8 autosomes.af.vcf.gz > roh.txt
+
+### ROH PER INDIVIDUAL
+awk '$1=="RG"{sum[$2]+=$6} END{for (i in sum) print i, sum[i]}' roh.txt > roh_total_bp.txt
+
+### COMPUTE FROH
+# NOTE: genome size includes Z; acceptable if documented
+GENOME_BP=1355857592
+
+awk -v G=$GENOME_BP '{print $1, $2/G}' roh_total_bp.txt > froh.txt
+
+### MERGE WITH POPULATION DATA
+cut -d',' -f1,2 buow_withpop.csv | tail -n +2 | sed 's/,/ /g' > id_pop.txt
+
+sort froh.txt > froh.sorted.txt
+sort id_pop.txt > id_pop.sorted.txt
+join froh.sorted.txt id_pop.sorted.txt > froh_with_pop.txt
+
+### POPULATION SUMMARY
+awk '{
+  pop=$3;
+  val=$2;
+  sum[pop]+=val;
+  sumsq[pop]+=val*val;
+  count[pop]+=1
+}
+END {
+  for (p in sum) {
+    mean=sum[p]/count[p];
+    var=(sumsq[p]/count[p])-(mean*mean);
+    sd=sqrt(var);
+    print p, count[p], mean, sd
+  }
+}' froh_with_pop.txt > pop_froh_summary.txt
+
+### FLAG SMALL GROUPS
+awk '{
+  if ($2 < 5) print $1"*", $2, $3, $4;
+  else print $1, $2, $3, $4;
+}' pop_froh_summary.txt > pop_froh_final.txt
